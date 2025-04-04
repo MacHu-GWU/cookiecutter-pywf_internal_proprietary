ea02c58f29bea09770b97f016840e46bcc4b22bd
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-03 20:57:04 -0400

    update template

--- s02_manage_github_release.py
+++ s02_manage_github_release.py
@@ -9,17 +9,38 @@ from s01_run_cookiecutter_maker import dir_here
 
 dir_tmp_template = dir_here / "tmp" / "{{ cookiecutter.package_name }}-project"
 dir_template = dir_here / "{{ cookiecutter.package_name }}-project"
+
 shutil.rmtree(dir_template, ignore_errors=True)
 shutil.copytree(dir_tmp_template, dir_template)
 
-args = [
-    "git", "add", "."
-]
-subprocess.run(args, cwd=dir_here, check=True)
-args = ["git", "commit", "-m", "update template"]
-try:
+
+def update_and_push_main():
+    args = ["git", "add", "."]
     subprocess.run(args, cwd=dir_here, check=True)
-except Exception as e:
-    print(e)
-args = ["git", "push"]
-subprocess.run(args, cwd=dir_here, check=True)
+
+    args = ["git", "commit", "-m", "update template"]
+    try:
+        subprocess.run(args, cwd=dir_here, check=True)
+    except Exception as e:
+        print(e)
+
+    args = ["git", "push"]
+    subprocess.run(args, cwd=dir_here, check=True)
+
+
+def merge_and_push_branch(branch_name: str):
+    args = ["git", "checkout", branch_name]
+    subprocess.run(args, cwd=dir_here, check=True)
+    args = ["git", "merge", "main"]
+    subprocess.run(args, cwd=dir_here, check=True)
+    args = ["git", "push"]
+    subprocess.run(args, cwd=dir_here, check=True)
+
+
+update_and_push_main()
+branch_name_list = [
+    "esc-sanhe-dev",
+    "bmt-sanhe-dev",
+]
+for branch_name in branch_name_list:
+    merge_and_push_branch(branch_name=branch_name)
